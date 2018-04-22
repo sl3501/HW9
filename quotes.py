d27658d9cd4855c948cb7c4c0f010d4c3b4a74eb
Shuoyang Li <sl3501@columbia.edu>
2018-04-22 07:43:22 +0000

    files

---
+++ quotes.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def quotations():
+    server = 'My server'
+
+    today_quote = 
+    return 
